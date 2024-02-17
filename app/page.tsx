d80dddf1aaf5a741ac23ea35adc17bfb87a1a6be
karm <karmjot.girn@mail.utoronto.ca>
2024-02-17 18:13:03 -0500

    added html

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,10 @@ import { currentUser } from "@/lib/data/user";
 
 export default async function Home() {
   const user = await currentUser();
-
   return (
+
+    <button>{title}</button>
+    
     <main>
       {user?.name || "No user"}
       <LogoutButton />
