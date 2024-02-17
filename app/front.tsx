e85ef7d4248161fdc754a0ab17cea8e8111f477b
karm <karmjot.girn@mail.utoronto.ca>
2024-02-17 18:56:00 -0500

    html

---
+++ app/front.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App() {
+    return (
+      <div className="App">
+        <h1>Hello World!</h1>
+      </div>
+    );
+  }
+  
+  export default App;
