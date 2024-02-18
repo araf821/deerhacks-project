c8b340f2c16cda674847757e7e8c8b486f074adb
karm <karmjot.girn@mail.utoronto.ca>
2024-02-17 19:29:00 -0500

    finish prog for now

--- app/page.tsx
+++ app/page.tsx
@@ -9,4 +9,16 @@ export default async function Home() {
       <LogoutButton />
     </main>
   );
+  
 }
+
+
+function App() {
+    return (
+      <div className="App">
+        <h1>Hello World!</h1>
+      </div>
+    );
+  }
+  
+  export default App;
