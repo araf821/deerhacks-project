33d9e2e7e48a04d6454c4d0755d7b351b1f07cea
araf821 <araf.ahmed200@gmail.com>
2024-02-18 04:51:44 -0500

    chore: recreate landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,45 @@
-import input_image from "./Edumates.png";
+import Link from "next/link";
 
-import "./style.css";
 import Image from "next/image";
-import { redirect } from "next/navigation";
+import { cn } from "@/lib/utils";
+import { port } from "@/lib/font";
 
 export default async function Home() {
   return (
-    <div>
-      <div className="center"></div>
+    <div className={cn("relative h-full", port.className)}>
       <Image
-        alt=""
-        width={808}
-        height={371}
-        className="mx-auto"
-        src={input_image}
-      />{" "}
-      <div />
-      <div className="center">
-        <form
-          action={async () => {
-            "use server";
-            redirect("/auth/login");
-          }}
-        >
-          <button type="submit">Log in</button>
-        </form>
-      </div>
-      <div className="center">
-        <form
-          action={async () => {
-            "use server";
-            redirect("/auth/register");
-          }}
-        >
-          <button type="submit">Sign up</button>
-        </form>
+        alt="landing page background"
+        sizes="100vw"
+        fill
+        className="-z-10 object-cover"
+        src={"/landing.webp"}
+      />
+
+      <div className="mx-auto w-fit px-4 pt-40">
+        <Image
+          src="/edumates.webp"
+          alt="edumates logo"
+          width={600}
+          height={100}
+        />
+        <h2 className="py-8 text-center text-2xl md:text-3xl lg:text-4xl">
+          Find Your Perfect Study Match
+        </h2>
+
+        <div className="flex items-center justify-center gap-4 max-sm:flex-col sm:gap-12 md:gap-16 lg:gap-20 xl:gap-24">
+          <Link
+            className="w-40 rounded-xl border-2 border-zinc-800 bg-zinc-800 px-4 py-2 text-center text-xl text-white transition hover:scale-125 max-sm:w-56 md:text-2xl"
+            href="/auth/login"
+          >
+            Log in
+          </Link>
+          <Link
+            className="w-40 rounded-xl border-2 border-black bg-white px-4 py-2 text-center text-xl text-black transition hover:scale-125 max-sm:w-56 md:text-2xl"
+            href="/auth/register"
+          >
+            Get Started
+          </Link>
+        </div>
       </div>
     </div>
   );
